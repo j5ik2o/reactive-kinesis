e8b6b49400e134063e64095b02cb7658274940b2
Junichi Kato <j5ik2o@gmail.com>
2019-02-24 10:18:26 +0900

    wip

--- core/src/main/scala/com/github/j5ik2o/reactive/kinesis/model/ListStreamsRequest.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/kinesis/model/ListStreamsRequest.scala
@@ -1,3 +1,8 @@
 package com.github.j5ik2o.reactive.kinesis.model
 
-class ListStreamsRequest {}
+final case class ListStreamsRequest(limit: Option[Int] = None, exclusiveStartStreamName: Option[String] = None) {
+
+  def withLimit(value: Option[Int]): ListStreamsRequest                       = copy(limit = value)
+  def withExclusiveStartStreamName(value: Option[String]): ListStreamsRequest = copy(exclusiveStartStreamName = value)
+
+}
